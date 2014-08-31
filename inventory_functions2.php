24570f89c109ad2ae273f0450d2177be64aec747
Tegan Brennan <tbrennan@tegans-air.home>
2014-08-30 23:25:49 -0400

    updates

--- inventory_functions2.php
+++ inventory_functions2.php
@@ -1,9 +1,7 @@
 <?php
 function newUser($id, $firstName, $lastName, $address, $phone, $email, $radius, $timeDelay, 
 	$host_desire, $host){
-	//in reality, this data would be coming from a database
-	//$items = array('12345'=>6789,'19283'=>100,'23489'=>'234');
-	//return $items[$id];
+
 	$user_name = "root";
 	$password = "root";
 	$database = "Initial_Chewie";
@@ -19,20 +17,18 @@ function newUser($id, $firstName, $lastName, $address, $phone, $email, $radius, 
 			$timeDelay, $host_desire, $host)";
 		$result = mysql_query($SQL);
 		mysql_close($db_handle);
-		return 1;
 
 	}
 	else {
 
 		print "Database NOT Found ";
 		mysql_close($db_handle);
-		return 0;
 
 	} 
 }
 
 
-function getUser($id){
+function existsUser($id){
 	$user_name = "root";
 	$password = "root";
 	$database = "Initial_Chewie";
@@ -42,10 +38,16 @@ function getUser($id){
 	$db_found = mysql_select_db($database, $db_handle); 
     if($db_found){
 		
-		$SQL = "SELECT * FROM PERSONS WHERE id =$id";
+		$SQL = "SELECT * FROM PERSONS WHERE id = $id LIMIT 1";
 		$result = mysql_query($SQL);
-		return 3;
+		if(mysql_num_rows($result) > 0){
+			return 1;		
+		}
 
+		else{
+			return 0;
+		} 
+		mysql_close($db_handle);
 	}
 	else {
 
@@ -55,4 +57,297 @@ function getUser($id){
 
 	} 
 }
+
+
+
+function getUserfirstName($id){
+	$user_name = "root";
+	$password = "root";
+	$database = "Initial_Chewie";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle); 
+    if($db_found){
+		
+		$SQL = "SELECT * FROM PERSONS WHERE id = $id LIMIT 1";
+		$result = mysql_query($SQL);
+		if(mysql_num_rows($result) > 0){
+			$row = mysql_fetch_row($result);
+			return $row[1];		
+		}
+
+		else{
+			$null = "null";
+			return $null;
+		} 
+		mysql_close($db_handle);
+	}
+	else {
+
+		print "Database NOT Found ";
+		mysql_close($db_handle);
+		return "Entry not found";
+
+	} 
+}
+
+
+function getUserlastName($id){
+	$user_name = "root";
+	$password = "root";
+	$database = "Initial_Chewie";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle); 
+    if($db_found){
+		
+		$SQL = "SELECT * FROM PERSONS WHERE id = $id LIMIT 1";
+		$result = mysql_query($SQL);
+		if(mysql_num_rows($result) > 0){
+			$row = mysql_fetch_row($result);
+			return $row[2];		
+		}
+
+		else{
+			$null = "null";
+			return $null;
+		} 
+		mysql_close($db_handle);
+	}
+	else {
+
+		print "Database NOT Found ";
+		mysql_close($db_handle);
+		return "Entry not found";
+
+	} 
+}
+
+function getUserAddress($id){
+	$user_name = "root";
+	$password = "root";
+	$database = "Initial_Chewie";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle); 
+    if($db_found){
+		
+		$SQL = "SELECT * FROM PERSONS WHERE id = $id LIMIT 1";
+		$result = mysql_query($SQL);
+		if(mysql_num_rows($result) > 0){
+			$row = mysql_fetch_row($result);
+			return $row[3];		
+		}
+
+		else{
+			$null = "null";
+			return $null;
+		} 
+		mysql_close($db_handle);
+	}
+	else {
+
+		print "Database NOT Found ";
+		mysql_close($db_handle);
+		return "Entry not found";
+
+	} 
+}
+
+
+function getUserPhone($id){
+	$user_name = "root";
+	$password = "root";
+	$database = "Initial_Chewie";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle); 
+    if($db_found){
+		
+		$SQL = "SELECT * FROM PERSONS WHERE id = $id LIMIT 1";
+		$result = mysql_query($SQL);
+		if(mysql_num_rows($result) > 0){
+			$row = mysql_fetch_row($result);
+			return $row[4];		
+		}
+
+		else{
+			$null = "null";
+			return $null;
+		} 
+		mysql_close($db_handle);
+	}
+	else {
+
+		print "Database NOT Found ";
+		mysql_close($db_handle);
+		return "Entry not found";
+
+	} 
+}
+
+function getUserEmail($id){
+	$user_name = "root";
+	$password = "root";
+	$database = "Initial_Chewie";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle); 
+    if($db_found){
+		
+		$SQL = "SELECT * FROM PERSONS WHERE id = $id LIMIT 1";
+		$result = mysql_query($SQL);
+		if(mysql_num_rows($result) > 0){
+			$row = mysql_fetch_row($result);
+			return $row[5];		
+		}
+
+		else{
+			$null = "null";
+			return $null;
+		} 
+		mysql_close($db_handle);
+	}
+	else {
+
+		print "Database NOT Found ";
+		mysql_close($db_handle);
+		return "Entry not found";
+
+	} 
+}
+
+
+function getUserRadius($id){
+	$user_name = "root";
+	$password = "root";
+	$database = "Initial_Chewie";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle); 
+	if($db_found){
+		
+		$SQL = "SELECT * FROM PERSONS WHERE id = $id LIMIT 1";
+		$result = mysql_query($SQL);
+		if(mysql_num_rows($result) > 0){
+			$row = mysql_fetch_row($result);
+			return $row[6];		
+		}
+
+		else{
+			$null = "null";
+			return $null;
+		} 
+		mysql_close($db_handle);
+	}
+	else {
+
+		print "Database NOT Found ";
+		mysql_close($db_handle);
+		return "Entry not found";
+
+	} 
+    
+}
+
+function getUserTimeDelay($id){
+	$user_name = "root";
+	$password = "root";
+	$database = "Initial_Chewie";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle); 
+	if($db_found){
+		
+		$SQL = "SELECT * FROM PERSONS WHERE id = $id LIMIT 1";
+		$result = mysql_query($SQL);
+		if(mysql_num_rows($result) > 0){
+			$row = mysql_fetch_row($result);
+			return $row[7];		
+		}
+
+		else{
+			$null = "null";
+			return $null;
+		} 
+		mysql_close($db_handle);
+	}
+	else {
+
+		print "Database NOT Found ";
+		mysql_close($db_handle);
+		return "Entry not found";
+
+	} 
+    
+}
+
+
+function getUserHostDesire($id){
+	$user_name = "root";
+	$password = "root";
+	$database = "Initial_Chewie";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle); 
+	if($db_found){
+		
+		$SQL = "SELECT * FROM PERSONS WHERE id = $id LIMIT 1";
+		$result = mysql_query($SQL);
+		if(mysql_num_rows($result) > 0){
+			$row = mysql_fetch_row($result);
+			return $row[8];		
+		}
+
+		else{
+			return 0;
+		} 
+		mysql_close($db_handle);
+	}
+	else {
+
+		print "Database NOT Found ";
+		mysql_close($db_handle);
+		return "Entry not found";
+	}    
+}
+
+function getUserHost($id){
+	$user_name = "root";
+	$password = "root";
+	$database = "Initial_Chewie";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle); 
+	if($db_found){
+		
+		$SQL = "SELECT * FROM PERSONS WHERE id = $id LIMIT 1";
+		$result = mysql_query($SQL);
+		if(mysql_num_rows($result) > 0){
+			$row = mysql_fetch_row($result);
+			return $row[9];
+		}
+
+		else{
+			return 0;
+		} 
+		mysql_close($db_handle);
+	}
+	else {
+
+		print "Database NOT Found ";
+		mysql_close($db_handle);
+		return "Entry not found";
+	}    
+}
+
 ?>
--- server2.php
+++ server2.php
@@ -4,5 +4,20 @@ require 'inventory_functions2.php';
 ini_set("soap.wsdl_cache_enabled", "0"); // disabling WSDL cache
 $server = new SoapServer("inventory2.wsdl");
 $server->addFunction("newUser");
+
+$server->addFunction("getUserfirstName");
+$server->addFunction("getUserlastName");
+$server->addFunction("getUserAddress");
+$server->addFunction("getUserPhone");
+$server->addFunction("getUserEmail");
+$server->addFunction("getUserRadius");
+$server->addFunction("getUserTimeDelay");
+$server->addFunction("getUserHostDesire");
+$server->addFunction("getUserHost");
+$server->addFunction("existsUser");
+
+
+
+
 $server->handle();
 ?>
