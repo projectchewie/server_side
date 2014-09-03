6ddf21ddfa70384ae69747959b0a7bfa08e363a0
Tegan Brennan <tbrennan@Tegans-MacBook-Air.local>
2014-09-03 15:10:18 -0400

    updates

--- client-test2.php
+++ client-test2.php
@@ -1,13 +1,22 @@
 <?php
     ini_set("soap.wsdl_cache_enabled", "0"); // disabling WSDL cache
-    $client = new SoapClient("http://localhost:8888/test/wsdl/inventory2.wsdl");
+    $client = new SoapClient("http://localhost:8888/test/wsdl/persons.wsdl");
     //$return = $client->newUser('12319', "Tegan", "Brennan", "here", '2403937283', 
     //   	'tsbrenna@gmail.com', '4', '5', '1', '0');
     $return = $client->existsUser('12347');
 	print_r($return);
 	$return = $client->updateUser('12347', 'host','0');
-    $return = $client->getUser('12345', 'firstName');
-    print_r($return)
+    $return = $client->getUser('12345', 'lastName');
+    print_r($return);
+    //$return = $client->newCart('12345', '12', '40', '7','1','0','0');
+    //print_r($return);
+    $return = $client->existsCart('12345', '12');
+    print_r($return);
+
+    $return = $client->getCart('12345', '12', 'num');
+    print_r($return);
+    $return = $client->updateCart('12345', '12','num', '7');
+
 
 
 
--- server2.php
+++ server2.php
@@ -1,10 +1,15 @@
 <?php
 require 'person_functions.php';
+require 'cart_functions.php';
 ini_set("soap.wsdl_cache_enabled", "0"); // disabling WSDL cache
 $server = new SoapServer("persons.wsdl");
 $server->addFunction("newUser");
 $server->addFunction("existsUser");
 $server->addFunction("updateUser");
 $server->addFunction("getUser");
+$server->addFunction("newCart");
+$server->addFunction("existsCart");
+$server->addFunction("getCart");
+$server->addFunction("updateCart");
 $server->handle();
 ?>
